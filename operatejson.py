d66d0ac2b087138eed41592574bbed1520b66660
jhinyao <jhinyao@163.com>
2019-05-24 18:06:35 +0800

    first

---
+++ operatejson.py
@@ -0,0 +1,24 @@
+import json
+
+#加载Json文件的模块
+class OperateJson:
+	def __init__(self, filePath):
+		self.filePath_ = filePath
+
+	def loadJson(self):
+
+		with open(self.filePath_, 'r') as load_f:
+			jsonStr = load_f.read()
+			self.dic = json.loads(jsonStr, strict=False) 
+		
+		return self.dic 
+
+
+	def dumpJson(self,dic):
+		with open(self.filePath_, 'w') as dump_f:
+			jsonStr = json.dumps(dic)
+			dump_f.write(jsonStr)
+
+
+
+
