48e764e3e9f82e43507ddeef7f7a03eff2803ece
Lazy <Lazy Yao>
2020-03-17 08:11:06 +0800

    modify

--- restructure/data/basictable.py
+++ restructure/data/basictable.py
@@ -2,6 +2,8 @@
 
 class BasicTable(object):
 
+	_INT = ["blur", ]
+	_FLOAT = ["left_percentage", "right_percentage","top_percentage","bottom_percentage","left_constant","right_constant","top_constant", "bottom_constant", "height_percentage", "height_constant","width_percentage","width_constant","filterStrength","rotation"]
 	def __init__(self):
 		super(BasicTable, self).__init__()
 
@@ -14,7 +16,11 @@ class BasicTable(object):
     				tableWidget.setItem(i, j, QTableWidgetItem(lambda lis[i][j]: lis[i][j] if isinstance(lis[i][j], str) else str(lis[i][j])))
 
 	def getTableItem(self, tableWidget):
-    	pass
+		lis = []
+    	for i in range(tableWidget.rowCount()):
+			for j in range(tableWidget.columnCount()):
+				lis[i][tableWidget.horizontalHeaderItem(j)] = tableWidget.item(i, j).text()
+		return lis
 	
 	
 	
