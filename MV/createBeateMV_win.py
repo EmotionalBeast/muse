ea541025f66fc0d5cd15963401f3b7740e30d5e5
Jhin Yao <jhinyao@163.com>
2020-03-30 08:16:29 +0800

    mv

---
+++ MV/createBeateMV_win.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   mv_on_mac.py
+@Time    :   2020/03/29 18:24:11
+@Author  :   Lazy Yao 
+@Version :   1.0
+@Contact :   jhinyao@163.com
+@License :   None yet
+@Desc    :   None
+'''
+
+# here put the import lib
+
+
+import os, subprocess, shutil, sys
+
+def encryption(path):
+    pass
+
+def createBeatMV(path):
+    pass
+
+def createChicMV(path):
+    pass
+
+def compass(path):
+    pass
+
+def cleanFile(path):
+    pass
+
+if __name__ == "__main__":
+    pass
